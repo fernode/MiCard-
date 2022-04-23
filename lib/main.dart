96ec2cbf086f9a11a0f5117e7b6662883b5dbea1
Fernando Silva <fernandosilvadj@hotmail.com>
2022-04-23 16:30:41 -0300

    feat: add first layout structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,50 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Center(
+              child: CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage('images/perfil.jpg'),
+              ),
+            ),
+            Text(
+              'Fernando Silva',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  color: Colors.white, fontFamily: 'Pacifico', fontSize: 18),
+            )
+          ],
+        )),
       ),
-    ),
-  );
+    );
+  }
+}
+
+class TextSection extends StatelessWidget {
+  Color _color;
+  TextSection(Color color) {
+    this._color = color;
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: this._color,
+      child: Text('Text some widget'),
+    );
+  }
 }
